4823656b383bb1f2ebbd2d8d9a87e7dc0cacabb1
Daniel Chadwick <chadanuk@web-feet.co.uk>
2019-08-08 07:16:42 +0100

    UPdate list view

--- resources/views/admin/pages/list.blade.php
+++ resources/views/admin/pages/list.blade.php
@@ -1,25 +1,26 @@
 <div class="mini-cms_list">
     <div class="mini-cms_header-wrapper">
         <h1 class="mini-cms__header">Pages</h1>
-        <a href="{{ route('mini-cms.pages.create') }}" class="mini-cms__button mini-cms__button--create">Create page</a>
+        <a href="{{ route('mini-cms.pages.create') }}"
+            class="p-2 border-rounded border-green mini-cms__button mini-cms__button--create">Create page</a>
     </div>
     <div class="mini-cms__table-wrapper">
         <table class="mini-cms__table">
             <thead>
                 <tr>
-                    <td>Name</td>
-                    <td>Actions</td>
+                    <th class="p-2">Name</th>
+                    <th class="p-2">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($pages as $page)
                 <tr>
-                    <td>{{ $page->name }}</td>
-                    <td>
+                    <td class="p-2">{{ $page->name }}</td>
+                    <td class="p-2">
                         <a href="{{ route('mini-cms.pages.edit', ['page' => $page->id]) }}" title="Edit page"
-                            class="mini-cms__button mini-cms__button--edit">Edit</a>
+                            class="p-2 border-rounded border-grey mini-cms__button mini-cms__button--edit">Edit</a>
                         <a href="{{ route('mini-cms.pages.delete', ['page' => $page->id]) }}" title="Delete page"
-                            class="mini-cms__button mini-cms__button--delete">Delete</a>
+                            class="p-2 border-rounded border-red  mini-cms__button mini-cms__button--delete">Delete</a>
                     </td>
                 </tr>
                 @endforeach
