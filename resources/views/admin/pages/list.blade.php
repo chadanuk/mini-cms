ee416c1e5e7406d6eb1fc878eb2ce259577cfb76
Daniel Chadwick <chadanuk@web-feet.co.uk>
2019-08-08 07:22:57 +0100

    UPdate list styles

--- resources/views/admin/pages/list.blade.php
+++ resources/views/admin/pages/list.blade.php
@@ -1,8 +1,8 @@
-<div class="mini-cms_list">
-    <div class="mini-cms_header-wrapper">
+<div class="mini-cms_list px-2">
+    <div class="mini-cms_header-wrapper flex justify-between">
         <h1 class="font-regular mini-cms__header">Pages</h1>
         <a href="{{ route('mini-cms.pages.create') }}"
-            class="p-2 border-rounded border-green mini-cms__button mini-cms__button--create">Create page</a>
+            class="p-2 border border-rounded border-green mini-cms__button mini-cms__button--create">Create page</a>
     </div>
     <div class="mini-cms__table-wrapper">
         <table class="mini-cms__table">
@@ -18,9 +18,9 @@
                     <td class="p-2">{{ $page->name }}</td>
                     <td class="p-2">
                         <a href="{{ route('mini-cms.pages.edit', ['page' => $page->id]) }}" title="Edit page"
-                            class="p-2 border-rounded border-grey mini-cms__button mini-cms__button--edit">Edit</a>
+                            class="p-2 border border-rounded border-grey mini-cms__button mini-cms__button--edit">Edit</a>
                         <a href="{{ route('mini-cms.pages.delete', ['page' => $page->id]) }}" title="Delete page"
-                            class="p-2 border-rounded border-red  mini-cms__button mini-cms__button--delete">Delete</a>
+                            class="p-2 border border-rounded border-red  mini-cms__button mini-cms__button--delete">Delete</a>
                     </td>
                 </tr>
                 @endforeach
